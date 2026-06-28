65a51649223bb92d794444c0595e071f9a138c76
PierreBader <34540557+PierreBader@users.noreply.github.com>
2026-06-28 16:12:35 +0200

    preload background

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -32,22 +32,32 @@ function cycleIndex() {
     background-repeat: no-repeat;
     background-size: cover;
 }
+.auth-background::after {
+    position: absolute;
+    width: 0;
+    height: 0;
+    overflow: hidden;
+    z-index: -1;
+    content: url(../assets/background1.webp) url(../assets/background2.webp)
+        url(../assets/background3.webp) url(../assets/background4.webp)
+        url(../assets/background5.webp) url(../assets/background6.webp);
+}
 .auth-background.bg1 {
-    background-image: url('../assets/background1.webp');
+    background-image: url(../assets/background1.webp);
 }
 .auth-background.bg2 {
-    background-image: url('../assets/background2.webp');
+    background-image: url(../assets/background2.webp);
 }
 .auth-background.bg3 {
-    background-image: url('../assets/background3.webp');
+    background-image: url(../assets/background3.webp);
 }
 .auth-background.bg4 {
-    background-image: url('../assets/background4.webp');
+    background-image: url(../assets/background4.webp);
 }
 .auth-background.bg5 {
-    background-image: url('../assets/background5.webp');
+    background-image: url(../assets/background5.webp);
 }
 .auth-background.bg6 {
-    background-image: url('../assets/background6.webp');
+    background-image: url(../assets/background6.webp);
 }
 </style>
